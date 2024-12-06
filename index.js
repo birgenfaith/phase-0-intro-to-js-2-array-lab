99b3816823f41cfa54f45e7c7e1074e5c935e97f
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-12-06 17:19:55 +0300

    add solutions on index.js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+destructivelyAppendCat("Ralph");
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+destructivelyRemoveLastCat();
+
+// REMOVE FIRST ELEMENT 
+function destructivelyRemoveFirstCat(){
+   cats.shift();
+}
+destructivelyRemoveFirstCat();
+
+// SPREAD OPERATOR 
+function appendCat(name){
+    return [...cats, name];  
+}
+appendCat("Broom");
+
+// PREPEND 
+function prependCat(name) {
+    return [name, ...cats]; 
+}
+prependCat("Arnold");
+
+// .SLICE() 
+function removeLastCat(){
+    return [...cats.slice(0, cats.length - 1)];  
+}
+console.log(removeLastCat());  
+
+function removeFirstCat(){
+    return [...cats.slice(1)];  
+}
+console.log(removeFirstCat()); 
+
+console.log(cats);  
+
